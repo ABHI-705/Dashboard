a30fd5c1753369e2027ac1ce0ff09326c14e1daf
Abhinav Tripathi <111434086+ABHI-705@users.noreply.github.com>
2024-04-01 17:39:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 from pandas_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
 
 st.set_page_config(page_title="Suicides In India",
                    page_icon=":bar_chart:",
