6f8dfa8b9b0ebf3a6a07c90412eb385d01f3578f
Abhinav Tripathi <111434086+ABHI-705@users.noreply.github.com>
2024-04-01 18:22:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -72,5 +72,5 @@ st.plotly_chart(fig_age_group_deaths)
 # Statistics Report
 st.title("Statistics Report")
 st.header("Data Summary")
-profile = ProfileReport(df, title="Pandas Profiling Report", minimal=True)
+profile = ProfileReport(df)
 profile
